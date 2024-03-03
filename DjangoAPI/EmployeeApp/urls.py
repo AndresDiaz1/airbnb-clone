4dea4c6e51e76cba6fc0b62ea19214aba453d833
Andres Diaz <andiazvar@gmail.com>
2024-03-02 19:29:31 -0500

    added posts DRF

--- DjangoAPI/EmployeeApp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from EmployeeApp import views
-
-urlpatterns = [
-    path(r"^department/$", views.departmentAPI),
-    path(r"^department/([0-9]+)$", views.departmentAPI),
-]
--- django-rest/core/urls.py
+++ django-rest/core/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for DjangoAPI project.
+URL configuration for core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -16,11 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
-
-from django.conf.urls import include
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path(r"^", include("EmployeeApp.urls")),
+    path("", include("blog.urls", namespace="blog")),
+    path("api/", include("blog_api.urls", namespace="blog_api")),
 ]
